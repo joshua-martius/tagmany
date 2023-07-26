ee73a89f11b28e028237534aa4ded4f6141414be
Joshua Martius <joshua.martius@rwth-aachen.de>
2023-07-26 16:43:50 +0200

    fixed edge cases

--- main.ts
+++ main.ts
@@ -26,28 +26,32 @@ export default class TagManyPlugin extends Plugin {
 
 	async addTagsToNotes(tags: string[], folder: TFolder, includeSubfolders: boolean, counter: number[] = [0]) {
 		for (const note of folder.children) {
-		  if (note instanceof TFolder) {
-			if (includeSubfolders) await this.addTagsToNotes(tags, note, true, counter);
-			continue;
-		  }
-		  const noteContent = await this.app.vault.read(note as TFile);
-		  const regex = /^---\ntags:\s*\[(.*?)\]\n---$/gm;
-		  const regexResult = regex.exec(noteContent);
-		  const trimmedTags = tags.map(tag => tag.trim()); // Trim leading and trailing whitespaces
-	  
-		  if (regexResult && regexResult[1]) {
-			const oldTags = regexResult[1].split(",").map(tag => tag.trim()); // Trim existing tags
-			const newTags = [...new Set([...oldTags, ...trimmedTags])];
-			const newNoteContent = noteContent.replace(regex, `---\ntags: [${newTags.join(",")}]\n---`);
+			if (note instanceof TFolder) {
+				if (includeSubfolders) await this.addTagsToNotes(tags, note, true, counter);
+				continue;
+			}
+			const noteContent = await this.app.vault.read(note as TFile);
+			const regex = /^---\ntags:\s*\[(.*?)\]\n---$/gm;
+			const regexResult = regex.exec(noteContent);
+			const trimmedTags = tags.map(tag => tag.trim()); // Trim leading and trailing whitespaces
+
+			let newNoteContent = "";
+			if (noteContent.contains("---\ntags: []\n---")) {
+				newNoteContent = noteContent.replace("---\ntags: []\n---", `---\ntags: [${trimmedTags.join(",")}]\n---`);
+			}
+			else if (regexResult && regexResult[1]) {
+				const oldTags = regexResult[1].split(",").map(tag => tag.trim()).filter(tag => !trimmedTags.includes(tag)); // Trim existing tags and remove new tags that are already present
+				const newTags = [...new Set([...oldTags, ...trimmedTags])];
+				newNoteContent = noteContent.replace(regex, `---\ntags: [${newTags.join(",")}]\n---`);
+			}
+			else { // No tags yet, no frontmatter yet
+				newNoteContent = `---\ntags: [${trimmedTags.join(",")}]\n---\n\n${noteContent}`;
+			}
+
 			await this.app.vault.modify(note as TFile, newNoteContent);
 			counter[0]++;
-		  } else {
-			const newNoteContent = `---\ntags: [${trimmedTags.join(",")}]\n---\n\n${noteContent}`;
-			await this.app.vault.modify(note as TFile, newNoteContent);
-			counter[0]++;
-		  }
 		}
 		new Notification("TagMany", { body: `Added tags to ${counter[0]} notes!` });
-	  }	  
+	}
 }
 
