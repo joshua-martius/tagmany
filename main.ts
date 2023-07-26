2e90c388063d8377ab3799391de48c8fea9c527b
Joshua Martius <joshua.martius@rwth-aachen.de>
2023-07-26 14:30:16 +0200

    base upload

--- main.ts
+++ main.ts
@@ -1,81 +1,43 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, TFolder } from 'obsidian';
+import { EnterTagsModal } from 'EnterTagsModal';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TagManyPlugin extends Plugin {
+	settings: TagManyPlugin;
 
 	async onload() {
 		await this.loadSettings();
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, folder) => {
+				if (!(folder instanceof TFolder)) return;
+				menu.addItem((item) => {
+					item
+						.setTitle("Tag all Notes in this Folder")
+						.setIcon("tags")
+						.onClick(async () => {
+							new EnterTagsModal(this.app, async (result) => {
+								const tags = result.split(",");
+								for (const note of folder.children) {
+									if (note instanceof TFolder) continue;
+									const noteContent = await this.app.vault.read(note as TFile);
+									const regex = /^---\ntags:\s*\[(.*?)\]\n---$/gm; // Note the "g" flag for global matching
+									const regexResult = regex.exec(noteContent); // Use exec instead of match for capturing groups
+									if (regexResult && regexResult[1]) {
+										const oldTags = regexResult[1].split(",");
+										const newTags = [...new Set([...oldTags, ...tags])];
+										const newNoteContent = noteContent.replace(regex, `---\ntags: [${newTags.join(",")}]\n---`);
+										await this.app.vault.modify(note as TFile, newNoteContent);
+									} else {
+										const newNoteContent = `---\ntags: [${tags.join(",")}]\n---\n\n${noteContent}`;
+										await this.app.vault.modify(note as TFile, newNoteContent);
+									}
+									new Notification("TagMany", { body: `Added tags to ${folder.children.length} notes!` });
+								}
+							}).open();
+						});
+				});
+			})
+		);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -83,52 +45,9 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
 	async saveSettings() {
-		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
