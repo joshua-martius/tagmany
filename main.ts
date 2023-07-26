dc4e5cc4a5ea8b6dbe3ec3779e9c61256b1be279
Joshua Martius <joshua.martius@rwth-aachen.de>
2023-07-26 15:24:22 +0200

    added recursion

--- main.ts
+++ main.ts
@@ -13,9 +13,9 @@ export default class TagManyPlugin extends Plugin {
 						.setTitle("Tag all Notes in this Folder")
 						.setIcon("tags")
 						.onClick(async () => {
-							new EnterTagsModal(this.app, async (result) => {
-								if (result) {
-									await this.addTagsToNotes(result.split(","), folder);
+							new EnterTagsModal(this.app, async (tags, includeSubfolders) => {
+								if (tags) {
+									await this.addTagsToNotes(tags.split(","), folder, includeSubfolders);
 								}
 							}).open();
 						});
@@ -24,23 +24,30 @@ export default class TagManyPlugin extends Plugin {
 		);
 	}
 
-	async addTagsToNotes(tags: string[], folder: TFolder) {
+	async addTagsToNotes(tags: string[], folder: TFolder, includeSubfolders: boolean, counter: number[] = [0]) {
 		for (const note of folder.children) {
-			if (note instanceof TFolder) continue;
-			const noteContent = await this.app.vault.read(note as TFile);
-			const regex = /^---\ntags:\s*\[(.*?)\]\n---$/gm;
-			const regexResult = regex.exec(noteContent);
-			if (regexResult && regexResult[1]) {
-				const oldTags = regexResult[1].split(",");
-				const newTags = [...new Set([...oldTags, ...tags])];
-				const newNoteContent = noteContent.replace(regex, `---\ntags: [${newTags.join(",")}]\n---`);
-				await this.app.vault.modify(note as TFile, newNoteContent);
-			} else {
-				const newNoteContent = `---\ntags: [${tags.join(",")}]\n---\n\n${noteContent}`;
-				await this.app.vault.modify(note as TFile, newNoteContent);
-			}
-			new Notification("TagMany", { body: `Added tags to ${folder.children.length} notes!` });
+		  if (note instanceof TFolder) {
+			if (includeSubfolders) await this.addTagsToNotes(tags, note, true, counter);
+			continue;
+		  }
+		  const noteContent = await this.app.vault.read(note as TFile);
+		  const regex = /^---\ntags:\s*\[(.*?)\]\n---$/gm;
+		  const regexResult = regex.exec(noteContent);
+		  const trimmedTags = tags.map(tag => tag.trim()); // Trim leading and trailing whitespaces
+	  
+		  if (regexResult && regexResult[1]) {
+			const oldTags = regexResult[1].split(",").map(tag => tag.trim()); // Trim existing tags
+			const newTags = [...new Set([...oldTags, ...trimmedTags])];
+			const newNoteContent = noteContent.replace(regex, `---\ntags: [${newTags.join(",")}]\n---`);
+			await this.app.vault.modify(note as TFile, newNoteContent);
+			counter[0]++;
+		  } else {
+			const newNoteContent = `---\ntags: [${trimmedTags.join(",")}]\n---\n\n${noteContent}`;
+			await this.app.vault.modify(note as TFile, newNoteContent);
+			counter[0]++;
+		  }
 		}
-	}
+		new Notification("TagMany", { body: `Added tags to ${counter[0]} notes!` });
+	  }	  
 }
 
