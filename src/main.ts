996116bf5b4f60a2abf621b0af9dc7aba9be255d
Joshua Martius <joshua.martius@rwth-aachen.de>
2023-07-29 13:43:00 +0200

    refactored tag adding

--- src/main.ts
+++ src/main.ts
@@ -35,25 +35,11 @@ export default class TagManyPlugin extends Plugin {
 				if (includeSubfolders) await this.addTagsToNotes(tags, note, true, counter);
 				continue;
 			}
-			const noteContent = await this.app.vault.read(note as TFile);
-			const regex = /^---\ntags:\s*\[(.*?)\]\n---$/gm;
-			const regexResult = regex.exec(noteContent);
-			const trimmedTags = tags.map(tag => tag.trim()); // Trim leading and trailing whitespaces
-
-			let newNoteContent = "";
-			if (noteContent.contains("---\ntags: []\n---")) {
-				newNoteContent = noteContent.replace("---\ntags: []\n---", `---\ntags: [${trimmedTags.join(",")}]\n---`);
-			}
-			else if (regexResult && regexResult[1]) {
-				const oldTags = regexResult[1].split(",").map(tag => tag.trim()).filter(tag => !trimmedTags.includes(tag)); // Trim existing tags and remove new tags that are already present
-				const newTags = [...new Set([...oldTags, ...trimmedTags])];
-				newNoteContent = noteContent.replace(regex, `---\ntags: [${newTags.join(",")}]\n---`);
-			}
-			else { // No tags yet, no frontmatter yet
-				newNoteContent = `---\ntags: [${trimmedTags.join(",")}]\n---\n\n${noteContent}`;
-			}
-
-			await this.app.vault.modify(note as TFile, newNoteContent);
+			
+			this.app.fileManager.processFrontMatter(note as TFile, (frontmatter) => {
+				frontmatter.tags = [...new Set([...frontmatter.tags, ...tags])];
+			})
+			
 			counter[0]++;
 		}
   }
